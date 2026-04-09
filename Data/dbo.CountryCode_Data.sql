5ffaf08444041fe5e9987596c52757cae8a879d2
Mahesh Gawsekar <mahesh@example.com>
2026-04-09 07:19:04 +0000

    static data

---
+++ Data/dbo.CountryCode_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[CountryCode] ON
+INSERT INTO [dbo].[CountryCode] ([ID], [CountryName], [CountryCode]) VALUES (1, N'India', N'IN')
+INSERT INTO [dbo].[CountryCode] ([ID], [CountryName], [CountryCode]) VALUES (2, N'United States', N'US')
+INSERT INTO [dbo].[CountryCode] ([ID], [CountryName], [CountryCode]) VALUES (3, N'United Kingdom', N'GB')
+SET IDENTITY_INSERT [dbo].[CountryCode] OFF

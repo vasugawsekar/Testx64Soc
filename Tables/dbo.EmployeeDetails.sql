6036cf57692821f74f9d7067e3445616f27a567c
vasugawsekar <vasugawsekar@gmail.com>
2026-04-09 07:26:38 +0000

    Update dbo.EmployeeDetails.sql

--- Tables/dbo.EmployeeDetails.sql
+++ Tables/dbo.EmployeeDetails.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[EmployeeDetails]
 [ID] [int] NOT NULL IDENTITY(1, 1),
 [Name] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [LastName] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[NickLastName] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CountryID] [int] NULL
 ) ON [PRIMARY]
 GO
